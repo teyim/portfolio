6f962dc33af501c1fa58d41fb576a6be4a48cf77
teyim <teyimasobo@gmail.com>
2022-11-10 08:25:05 +0100

    refactor: modify index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next';
+import Cell, { cellSize } from '@components/cell';
+import Grid from '@components/grid';
+import Navbar from '@components/global/navbar';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <section className="font-SG overflow-hidden text-gray-300 ">
+      <Grid />
+      <Navbar />
+    </section>
   );
 };
 
