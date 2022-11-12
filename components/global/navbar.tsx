3ae0bf297f4319af7b238357934768e81b2e7d93
teyim <teyimasobo@gmail.com>
2022-11-12 10:11:05 +0100

    refactor: modify navbar.tsx

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -3,16 +3,20 @@ import { routes } from 'data/global';
 import Link from 'next/link';
 import { IoMenuOutline } from 'react-icons/io5';
 import { MdClose } from 'react-icons/md';
+import { useRouter } from 'next/router';
 
 function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const router = useRouter();
+  const { route: currentRoute } = router;
 
   const handleToggle = () => {
     setNavbarOpen((prev) => !prev);
   };
 
   return (
-    <div className="py-8 px-10 md:px-40">
+    <div className="py-8 px-10 md:px-40 z-100">
       <div className="flex justify-between">
         <Link href="/" passHref>
           <a className="font-lexend font-semibold text-2xl md:text-3xl gradient-text">
@@ -22,7 +26,12 @@ function Navbar() {
 
         <ul className="hidden w-3/5 md:flex justify-evenly my-auto text-xl text-gray-500 font-bold">
           {routes.map((route) => (
-            <li key={route.title} className="hover:gradient-text">
+            <li
+              key={route.title}
+              className={`hover:gradient-text ${
+                currentRoute === route.path ? 'gradient-text' : ''
+              }`}
+            >
               <Link href={route.path}>{route.title}</Link>
             </li>
           ))}
