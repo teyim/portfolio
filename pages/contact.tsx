461b318ed90069e70f3148f4915fb009b2e018d7
teyim <teyimasobo@gmail.com>
2022-11-27 17:01:16 +0100

    refactor(contact.tsx): fix typo

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,10 +3,10 @@ import PageStructure from 'HOC/pageStructure';
 function Contact() {
   return (
     <PageStructure heading="Contact" width="md:w-3/5 text-center">
-      <p className="my-5 font-normal text-2xl">Say Hi...</p>
+      <p className="my-5 font-normal text-2xl mt-10">Say Hi...</p>
       <a
-        className="font-SG underline text-3xl font-semibold "
-        href="mailo:teyimasobo@gmail.com"
+        className="font-SG underline text-3xl font-semibold hover:gradient-text"
+        href="mailto:teyimasobo@gmail.com "
       >
         teyimasobo@gmail.com
       </a>
