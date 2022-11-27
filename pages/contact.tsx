ddeabe66d5b1906c3a9989eb33e6cbaf9474ce9f
teyim <teyimasobo@gmail.com>
2022-11-27 17:39:23 +0100

    feat: add nextseo

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,16 +1,20 @@
 import PageStructure from 'HOC/pageStructure';
+import { NextSeo } from 'next-seo';
 
 function Contact() {
   return (
-    <PageStructure heading="Contact" width="md:w-3/5 text-center">
-      <p className="my-5 font-normal text-2xl mt-10">Say Hi...</p>
-      <a
-        className="font-SG underline text-3xl font-semibold hover:gradient-text"
-        href="mailto:teyimasobo@gmail.com "
-      >
-        teyimasobo@gmail.com
-      </a>
-    </PageStructure>
+    <>
+      <NextSeo title="Contact Me" />
+      <PageStructure heading="Contact" width="md:w-3/5 text-center">
+        <p className="my-5 font-normal text-2xl mt-10">Say Hi...</p>
+        <a
+          className="font-SG underline text-3xl font-semibold hover:gradient-text"
+          href="mailto:teyimasobo@gmail.com "
+        >
+          teyimasobo@gmail.com
+        </a>
+      </PageStructure>
+    </>
   );
 }
 
