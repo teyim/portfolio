eefde2f9f4c4e3f497fa1a437bddd13e8a00da0d
teyim <teyimasobo@gmail.com>
2022-11-27 16:54:47 +0100

    refactor(contact.tsx): modify

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import PageStructure from 'HOC/pageStructure';
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <PageStructure heading="Contact" width="md:w-3/5 text-center">
+      <p className="my-5 font-normal text-2xl">Say Hi...</p>
+      <a
+        className="font-SG underline text-3xl font-semibold "
+        href="mailo:teyimasobo@gmail.com"
+      >
+        teyimasobo@gmail.com
+      </a>
+    </PageStructure>
+  );
 }
 
 export default Contact;
