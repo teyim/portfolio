d075f255eb9c4b590f0c095ac08e1ac661db9d40
TEYIM ASOBO <48864977+teyim@users.noreply.github.com>
2023-07-17 11:34:06 +0100

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -1,8 +1,17 @@
 import { Project } from 'types';
 
 export const projects: Project[] = [
-  {
+    {
     id: 0,
+    title: 'Uba votes',
+    desc: 'An e-voting platform that allows registered university students to take part in school elections',
+    img: '/static/projects/markdown-blog.png',
+    link: 'https://uba-votes.vercel.app/',
+    github: 'https://github.com/teyim/Uba-votes',
+    tags: ['Nextjs', 'Tailwind', 'Nodejs', 'TYpescript',"React-query"],
+  },
+  {
+    id: 1,
     title: 'Markdown Blog',
     desc: 'A simple, yet fast markdown blog',
     img: '/static/projects/markdown-blog.png',
@@ -11,7 +20,7 @@ export const projects: Project[] = [
     tags: ['Nextjs', 'Tailwind', 'MDX', 'MongoDB'],
   },
   {
-    id: 1,
+    id: 2,
     title: 'Infolio',
     desc: 'A collection of well designed developer portfolios, to get inspiration from',
     img: '/static/projects/infolio.png',
@@ -20,7 +29,7 @@ export const projects: Project[] = [
     tags: ['Nextjs', 'Tailwind', 'Typescript', 'Sanity'],
   },
   {
-    id: 2,
+    id: 3,
     title: 'Cultured kid',
     desc: 'Platform to buy and sell arts works',
     img: '/static/projects/culturedkid.png',
@@ -29,7 +38,7 @@ export const projects: Project[] = [
     tags: ['Nextjs', 'Tailwind', 'Firebase', 'Zustand'],
   },
   {
-    id: 3,
+    id: 4,
     title: 'Walle',
     desc: 'A dynamic Readme image that changes based on github activity',
     img: '/static/projects/walle.png',
@@ -37,7 +46,7 @@ export const projects: Project[] = [
     tags: ['Nodejs', 'Github actions', 'Vercel-og'],
   },
   {
-    id: 4,
+    id: 5,
     title: 'Jadoo',
     desc: 'Frontend for a Bus ticket booking platform to reduce wait time at travelling agencies',
     img: '/static/projects/jadoo.png',
@@ -46,7 +55,7 @@ export const projects: Project[] = [
     tags: ['React', 'Redux', 'Tailwind', 'Firebase'],
   },
   {
-    id: 5,
+    id: 6,
     title: 'Portfolio Website',
     desc: 'My personal portfolio website',
     img: '/static/projects/portfolio.png',
